ad1ec11066bf64680becca92dde4d683f721a29f
quikc <j10.sterling@gmail.com>
2023-02-16 14:40:18 +0000

    migration to freqtrade

---
+++ freqaimodels/__init__.py
@@ -0,0 +1,8 @@
+
+from CatboostRegressor import CatboostRegressor
+from .LightGBMClassifier import LightGBMClassifier
+from .ReinforcementLearner import ReinforcementLearner
+from .XGBoostClassifier import XGBoostClassifier
+
+__all__ = ["CatboostRegressor", "LightGBMClassifier",
+           "ReinforcementLearner", "XGBoostClassifier"]
--- strategies/__init__.py
+++
@@ -1,4 +0,0 @@
-from .keltner import Keltner
-from .logReg import LogReg
-from .deepQ import DeepQ
-__all__ = ["Keltner", "LogReg", "DeepQ"]
