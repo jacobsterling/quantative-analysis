45809e04eec52d564f6cac58aa534859af7e77e7
quikc <j10.sterling@gmail.com>
2023-02-18 16:43:34 +0000

    update to classifier

--- freqaimodels/__init__.py
+++ freqaimodels/__init__.py
@@ -1,8 +1,9 @@
 
 from CatboostRegressor import CatboostRegressor
+from CatboostClassifier import CatboostClassifier
 from .LightGBMClassifier import LightGBMClassifier
 from .ReinforcementLearner import ReinforcementLearner
 from .XGBoostClassifier import XGBoostClassifier
 
 __all__ = ["CatboostRegressor", "LightGBMClassifier",
-           "ReinforcementLearner", "XGBoostClassifier"]
+           "ReinforcementLearner", "XGBoostClassifier", "CatboostClassifier"]
